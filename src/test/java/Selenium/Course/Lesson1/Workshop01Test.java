f7618aecc582cdc64849cee9e1cac3d56cfe6375
Pawel Bielenda <pbielenda@gmail.com>
2017-02-26 20:58:07 +0100

    Poprawne rozwiazanie

--- src/test/java/Selenium/Course/Lesson1/Workshop01Test.java
+++ src/test/java/Selenium/Course/Lesson1/Workshop01Test.java
@@ -1,145 +1,144 @@
 package Selenium.Course.Lesson1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Workshop01Test {
 	
 	private String testUrl = "https://suvroc.github.io/selenium-mail-course/01/workshop.html";
+	WebDriver driver ;
+	
+	@BeforeTest
+	public void initBrowser(){
+		driver = new ChromeDriver();
+	}
 	
 	@Test
 	public void ShouldTestFirstName(){
 		
-		WebDriver driver = new ChromeDriver();
 	    driver.get(testUrl);
 	    String value = "";
 	    WebElement element = null;
 	    
 	    //Code 
-	    
+	    element = driver.findElement(By.name("firstName"));
+	    value = element.getAttribute("value");
 	    //Code
 
 	    Assert.assertNotNull(element);
 	    Assert.assertTrue(element instanceof  WebElement);
 	    Assert.assertEquals(value, "Terry");
-
-	    driver.quit();
 	 }
 	
 	@Test
     public void ShouldTestLastName()
     {
-		WebDriver driver = new ChromeDriver();
 	    driver.get(testUrl);
 	    String value = "";
 	    WebElement element = null;
 	    
 	    //Code 
-	    
+	    element = driver.findElement(By.name("lastName"));
+	    value = element.getAttribute("value");
 	    //Code
 
 	    Assert.assertNotNull(element);
 	    Assert.assertTrue(element instanceof  WebElement);
 	    Assert.assertEquals(value, "Pratchett");
-
-	    driver.quit();
     }
 
     @Test
     public void ShouldTestCountry()
     {
-    	WebDriver driver = new ChromeDriver();
 	    driver.get(testUrl);
 	    String value = "";
 	    WebElement element = null;
 	    
 	    //Code 
-	    
+	    element = driver.findElement(By.name("country"));
+	    value = element.getAttribute("value");
 	    //Code
 
 	    Assert.assertNotNull(element);
 	    Assert.assertTrue(element instanceof  WebElement);
 	    Assert.assertEquals(value, "England");
-
-	    driver.quit();
     }
 
     @Test
     public void ShouldTestActiveCheckbox()
     {
-    	WebDriver driver = new ChromeDriver();
 	    driver.get(testUrl);
-	    String value = "";
+	    boolean value = true;
 	    WebElement element = null;
 
-//Code 
-	    
+	    //Code 
+	    element = driver.findElement(By.id("isActive"));
+	    value = element.isSelected();
 	    //Code
 
 	    Assert.assertNotNull(element);
 	    Assert.assertTrue(element instanceof  WebElement);
         Assert.assertEquals(element.isEnabled(), false);
         Assert.assertEquals(value, false);
-
-        driver.quit();
     }
 
     @Test
     public void ShouldTestCommentInput()
     {
-    	WebDriver driver = new ChromeDriver();
 	    driver.get(testUrl);
 	    String value = "";
 	    WebElement element = null;
 	    
 	    //Code 
-	    
+	    element = driver.findElement(By.id("commentInput"));
 	    //Code
 
 	    Assert.assertNotNull(element);
 	    Assert.assertTrue(element instanceof  WebElement);
 	    Assert.assertEquals(value, "");
-
-	    driver.quit();
     }
 
     @Test
     public void ShouldTestDetailsLink()
     {
-    	WebDriver driver = new ChromeDriver();
 	    driver.get(testUrl);
 	    WebElement element = null;
 	    
 	    //Code 
-	    
+	    element = driver.findElement(By.linkText("Details"));
 	    //Code
 
 	    Assert.assertNotNull(element);
 	    Assert.assertTrue(element instanceof  WebElement);
 	    Assert.assertEquals(element.getAttribute("href"), "https://en.wikipedia.org/wiki/Terry_Pratchett");
-
-	    driver.quit();
     }
 
     @Test
     public void ShouldTestListOfBooksLink()
     {
-    	WebDriver driver = new ChromeDriver();
 	    driver.get(testUrl);
 	    WebElement element = null;
 	    
 	    //Code 
-	    
+	    element = driver.findElement(By.linkText("List of books"));
 	    //Code
 
 	    Assert.assertNotNull(element);
 	    Assert.assertTrue(element instanceof  WebElement);
 	    Assert.assertEquals(element.getAttribute("href"), "https://www.terrypratchettbooks.com/books/");
 
-	    driver.quit();
+	    
+    }
+    
+    @AfterTest
+    public void terminateBrowser(){
+    	driver.quit();
     }
 	
 }
